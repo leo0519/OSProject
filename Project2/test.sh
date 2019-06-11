c8dfb374ff451c4f3125ad0f66eb7f63f6369646
leo0519 <39020268+leo0519@users.noreply.github.com>
2019-06-11 23:55:16 +0800

    update project2

---
+++ Project2/test.sh
@@ -0,0 +1,4 @@
+#sudo ./user_program/master ./data/file1.in m & sudo ./user_program/slave ./data/file1.out m 127.0.0.1
+#sudo ./user_program/master ./data/file2.in m & sudo ./user_program/slave ./data/file2.out m 127.0.0.1
+sudo ./user_program/master ./data/file3.in f & sudo ./user_program/slave ./data/file3.out f 127.0.0.1
+#sudo ./user_program/master ./data/file4.in m & sudo ./user_program/slave ./data/file4.out m 127.0.0.1
